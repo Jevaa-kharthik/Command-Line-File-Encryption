a79edc9f80a58b00c7a1a83f0e7e29f15de9c0ee
Jevaa Kharthik <jevaasureka@gmail.com>
2024-05-29 19:35:04 +0530

    Updated file with less time complexity

--- encrypto/lib/Argument.py
+++ encrypto/lib/Argument.py
@@ -1,47 +1,31 @@
 class Argument:
     def __init__(self, args):
-        self.commands = []  # --pathname=<FilePath>  --secret=<Password> --encryption=<WhatKindOfEncryption>
-        self.options = []  # <--pathname>   <--secret>  <--encryption>
+        self.commands = []
+        self.options = []
         self.optionValues = {}
-        # Dict/Set here... Unique Index/No Repeating Value
-        # <FilePath>  <Password>  <WhatKindOfEncryption>
-        self.args = args
-
-        for arg in self.args:
+        
+        for arg in args:
             if "=" in arg:
-                if "=" in arg:
-                    pair = arg.split()
-                    self.optionValues[pair[0]] = pair[1]
-                    # --pathname='I/Am/The/Path'
-                    # pair[0] = --pathname
-                    # pair[1] = 'I/Am/The/Path'
-                    self.options.append(pair[0])
-                else:
-                    self.options.append(arg) # pair[0] = --pathname
+                pair = arg.split('=')
+                self.optionValues[pair[0]] = pair[1]
+                self.options.append(pair[0])
             else:
                 self.commands.append(arg)
 
-    def hasOptions(self, options: list):
-        userOptions = set(self.options)
-        reqOptions = set(options)
-        return len(list(userOptions & reqOptions)) >= 1 # userOptions Union Of reqOptions shold be greater than 1 to print the function
+    def hasOptions(self, options):
+        return any(option in self.options for option in options)
 
     def hasOption(self, option):
-        return option in self.hasOptions([option]) # This loops to the hasOptions(self, options: list)
+        return option in self.options
 
     def hasCommands(self, commands):
-        userCommands = set(self.commands)
-        reqCommands = set(commands)
-        return list(userCommands & reqCommands) # This hasCommands(self, commands) will check wheather the command satisfies
+        return any(command in self.commands for command in commands)
 
     def hasCommand(self, command):
-        return command in self.hasCommands([command]) # This loops to the hasCommands(self, commands)
+        return command in self.commands
 
-    def getOptionValue(self, option, defalut=None):
-        if option in self.optionValues:
-            return self.optionVallues[option] # It will get the vaule of option from hasOptions(self,options)
-        else:
-            return defalut # else it returns default to the options value
+    def getOptionValue(self, option, default=None):
+        return self.optionValues.get(option, default)
 
-    def hasOptionvalue(self, option):
-        return option in self.optionValues # if it has the option value it will send to the self.optionvalues
+    def hasOptionValue(self, option):
+        return option in self.optionValues
