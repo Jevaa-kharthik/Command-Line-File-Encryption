51bfb133a6e1cb9cbaf24ed77c11d0970dcf7c3f
Jevaa Kharthik <jevaasureka@gmail.com>
2024-05-29 19:36:46 +0530

    Updated with features new hashes

--- encrypto/lib/Hashes.py
+++ encrypto/lib/Hashes.py
@@ -1,49 +1,27 @@
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.primitives.hashes import SHA1
-from cryptography.hazmat.primitives.hashes import SHA224
-from cryptography.hazmat.primitives.hashes import SHA256
-from cryptography.hazmat.primitives.hashes import SHA384
-from cryptography.hazmat.primitives.hashes import SHA512
-from cryptography.hazmat.primitives.hashes import SHA3_224
-from cryptography.hazmat.primitives.hashes import SHA3_256
-from cryptography.hazmat.primitives.hashes import SHA3_384
-from cryptography.hazmat.primitives.hashes import SHA3_512
-from cryptography.hazmat.primitives.hashes import SHA512_224
-from cryptography.hazmat.primitives.hashes import SHAKE128
-from lib.Argument import Argument
-import sys
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
 
 class Hashes:
-    def hash(en_algo):
-        a = Argument(sys.argv)
-        if a.hasOptions("--encryption"):
-            # en_algo = a.getOptionValue("--encryption")
-            try :
-                if(en_algo == "SHA1"):
-                    encrypted = SHA1()
-                elif(en_algo == "SHA224"):
-                    encrypted = SHA224()
-                elif(en_algo == "SHA256"):
-                    encrypted = SHA256()
-                elif(en_algo == "SHA3844"):
-                    encrypted = SHA384()
-                elif(en_algo == "SHA512"):
-                    encrypted = SHA512()
-                elif(en_algo == "SHA3_224"):
-                    encrypted = SHA3_224()
-                elif(en_algo == "SHA3_256"):
-                    encrypted = SHA3_256()
-                elif(en_algo == "SHA3_384"):
-                    encrypted = SHA3_384()
-                elif(en_algo == "SHA3_512"):
-                    encrypted = SHA3_512()
-                elif(en_algo == "SHA512_224"):
-                    encrypted = SHA512_224()
-                elif(en_algo == "SHAKE128"):
-                    encrypted = SHAKE128()
-                else :
-                    print("USAGE : There is no encryption called {}".format())
-                    
-            except Exception as e:
-                print("USAGE : There is no encryption called {}".format())
+    def hash(self, data, en_algo):
+        algorithms = {
+            "SHA1": hashes.SHA1(),
+            "SHA224": hashes.SHA224(),
+            "SHA256": hashes.SHA256(),
+            "SHA384": hashes.SHA384(),
+            "SHA512": hashes.SHA512(),
+            "SHA3_224": hashes.SHA3_224(),
+            "SHA3_256": hashes.SHA3_256(),
+            "SHA3_384": hashes.SHA3_384(),
+            "SHA3_512": hashes.SHA3_512(),
+            "SHA512_224": hashes.SHA512_224(),
+            "SHAKE128": hashes.SHAKE128(length=16),
+        }
+        
+        if en_algo not in algorithms:
+            raise ValueError(f"There is no encryption called {en_algo}")
+
+        digest = hashes.Hash(algorithms[en_algo], backend=default_backend())
+        digest.update(data.encode() if isinstance(data, str) else data)
+        return digest.finalize()
